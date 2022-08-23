7fed5c1217705af07b9b4f2feb72f06bd6382746
Shing Zhan <shing.zhan@gmail.com>
2022-08-23 10:24:04 +0100

    Remove get_sequence_length() and refactor

--- python/read_vcf.py
+++ python/read_vcf.py
@@ -1,23 +1,22 @@
+import click
 import numpy as np
 import tskit
 import tsinfer
 import cyvcf2
 
 
-def get_chromosome_length(vcf):
-    assert len(vcf.seqlens) == 1
-    return vcf.seqlens[0]
-
-
-def add_individuals(vcf, samples):
+def add_haploid_individuals(vcf, sample_data):
+    """
+    TODO: Parse anno file.
+    """
     for name in vcf.samples:
-        samples.add_individual(
+        sample_data.add_individual(
             ploidy = 1,
             metadata = {"name": name}
         )
 
 
-def add_haploid_sites(vcf, samples):
+def add_haploid_sites(vcf, sample_data):
     """
     Read the sites in the VCF and add them to the SampleData object,
     reordering the alleles to put the ancestral allele first, if it is available.
@@ -45,22 +44,30 @@ def add_haploid_sites(vcf, samples):
             allele_index[-1] = tskit.MISSING_DATA
             new_alleles += [None]
 
-        genotypes = [
-            allele_index[old_index]
-            for row in v.genotypes
-            for old_index in row[0:ploidy_level] # ???
-        ]
+        assert np.all(v.ploidy == 1)
+        new_genotypes = [allele_index[g[0]] for g in v.genotypes]
 
-        samples.add_haploid_site(pos, genotypes=genotypes, alleles=new_alleles)
+        sample_data.add_site(pos, genotypes=new_genotypes, alleles=new_alleles)
 
 
-def create_sample_data_from_vcf(in_file):
+@click.command()
+@click.option('--in_file', '-i', required=True, help="Input VCF/BCF file")
+@click.option('--out_file', '-o', required=True, help="Output samples file")
+def create_sample_data_from_vcf(in_file, out_file):
+    """
+    Convert a VCF/BCF file into a SampleData object,
+    and write it to a samples file for input to tsinfer.
+    """
     vcf = cyvcf2.VCF(in_file, strict_gt=True)
 
     with tsinfer.SampleData(
-        sequence_length = get_chromosome_length(vcf)
-    ) as samples:
-        add_individuals(vcf, samples)
-        add_haploid_sites(vcf, samples)
+        path=out_file
+    ) as sample_data:
+        add_haploid_individuals(vcf, sample_data)
+        add_haploid_sites(vcf, sample_data)
 
-    return(samples)
+    return(sample_data)
+
+
+if __name__ == "__main__":
+    create_sample_data_from_vcf()
